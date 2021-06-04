587c0717eba884d82fe83fa781401cb1590198f1
Olex <olexolex@gmx.de>
2021-06-04 22:02:04 +0200

    Minor map adaptions, added popup areas

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-WA.openPopup("popupRectangle", 'Dieser Server ist privat gehostet, soweit es hobbymäßig möglich ist wurden alle Maßnahmen getroffen, dass möglichst keine Personen- und IP-bezogenen Daten gespeichert werden. Für Details schau bitte in die aktuelle Dokumentation zu den Docker Containern von Workadventure und Jitsi', [{ label: "Verstanden", className: "primary", callback: (popup) => { WA.restorePlayerControls(); popup.close(); } }]);
+WA.openPopup("WelcomePopupRect", 'Dieser Server ist privat gehostet, soweit es hobbymäßig möglich ist wurden alle Maßnahmen getroffen, dass möglichst keine Personen- und IP-bezogenen Daten gespeichert werden. Für Details schau bitte in die aktuelle Dokumentation zu den Docker Containern von Workadventure und Jitsi', [{ label: "Verstanden", className: "primary", callback: (popup) => { WA.restorePlayerControls(); popup.close(); } }]);
 
 WA.onEnterZone('welcomesign', () => {
-    WA.disablePlayerControls();
-    WA.openPopup("popupRectangle", "Willkommen auf dem WorkAdventure Server von Olex\n Im Außenbereich (außer Raucherecke), den Gängen und den beiden südlichen großen Räumen kannst du dich mit Leuten in deiner Nähe unterhalten.\n Im Keller im Norden kann dich niemand stören.\n In den anderen Räumen können alle im Raum miteinander reden, in größeren Räumen musst du das Beitreten bestätigen.\n Viel Spaß!", [{ label: "Alles klar", className: "primary", callback: (popup) => { WA.restorePlayerControls(); popup.close(); } }]);
+    WA.openPopup("WelcomeSignPopupRect", "Willkommen auf dem WorkAdventure Server von Olex\n Im Außenbereich (außer Raucherecke), den Gängen und den beiden südlichen großen Räumen kannst du dich mit Leuten in deiner Nähe unterhalten.\n Im Keller im Norden kann dich niemand stören.\n In den anderen Räumen können alle im Raum miteinander reden, in größeren Räumen musst du das Beitreten bestätigen.\n Viel Spaß!", [{ label: "Alles klar", className: "primary", callback: (popup) => {  popup.close(); } }]);
     
 });
 
